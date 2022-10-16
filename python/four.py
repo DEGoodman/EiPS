5636c3da24b9435099c3487871503b00132b17e9
Erik Goodman <erik.goodman@agilebits.com>
2022-10-15 21:13:30 -0500

    copy-paste from book version - difference on line 20 'else' indent caused runtime error

--- python/four.py
+++ python/four.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import sys, string
 
 # the global list of [word, frequency] pairs
@@ -15,38 +15,35 @@ for line in open(sys.argv[1]):
     for c in line:
         if start_char == None:
             if c.isalnum():
-                # we found the start of a word
+                # We found the start of a word
                 start_char = i
-            else:
-                if not c.isalnum():
-                    # we found the end of a word, process it
-                    found = False
-                    word = line[start_char:i].lower()
-                    # ignore stop words
-                    if word not in stop_words:
-                        pair_index = 0
-                        # let's see if it already exists
-                        for pair in word_freqs:
-                            if word == pair[0]:
-                                pair[1] += 1
-                                found = True
-                                break
-                            pair_index += 1
-                        if not found:
-                            word_freqs.append([word, 1])
-                        elif len(word_freqs) > 1:
-                            # we may need to reorder
-                            for n in reversed(range(pair_index)):
-                                if word_freqs[pair_index][1] > \
-                                   word_freqs[n][1]:
-                                        # swap
-                                        word_freqs[n], word_freqs[\
-                                            pair_index] = word_freqs[\
-                                            pair_index], word_freqs[n]
-                                        pair_index = n
-                    # let's reset
-                    start_char = None
-            i += 1
+        else:
+            if not c.isalnum():
+                # We found the end of a word. Process it
+                found = False
+                word = line[start_char:i].lower()
+                # Ignore stop words
+                if word not in stop_words:
+                    pair_index = 0
+                    # Let's see if it already exists
+                    for pair in word_freqs:
+                        if word == pair[0]:
+                            pair[1] += 1
+                            found = True
+                            break
+                        pair_index += 1
+                    if not found:
+                        word_freqs.append([word, 1])
+                    elif len(word_freqs) > 1:
+                        # We may need to reorder
+                        for n in reversed(range(pair_index)):
+                            if word_freqs[pair_index][1] > word_freqs[n][1]:
+                                # swap
+                                word_freqs[n], word_freqs[pair_index] = word_freqs[pair_index], word_freqs[n]
+                                pair_index = n
+                # Let's reset
+                start_char = None
+        i += 1
 
 for tf in word_freqs[0:25]:
     print(tf[0], '-', tf[1])
