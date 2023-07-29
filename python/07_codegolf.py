a751f77a55b883e1fcf16eeea85808c9b4caf3a3
Erik Goodman <erik-gh@unoseeme.com>
2023-07-29 17:34:16 -0500

    add 7 formatted

--- python/07_codegolf.py
+++ python/07_codegolf.py
@@ -10,11 +10,11 @@ import time
 # runtime calc
 start_time = time.time()
 
-stops = open('../static/stop_words.txt').read().split(',')
-words = re.findall('[a-z]{2,}', open(sys.argv[1]).read().lower())
+stops = open("../static/stop_words.txt").read().split(",")
+words = re.findall("[a-z]{2,}", open(sys.argv[1]).read().lower())
 counts = collections.Counter(w for w in words if w not in stops)
 for (w, c) in counts.most_common(25):
-    print(w, '-', c)
+    print(w, "-", c)
 
 # final runtime calc
 print("--- %s seconds ---" % (time.time() - start_time))
